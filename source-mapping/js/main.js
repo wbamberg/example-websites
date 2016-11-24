40275f4386a9208a07350535cf8d509001358a72
Will Bamberg <wbamberg@mozilla.com>
2016-11-24 09:09:09 -0800

    use consistent quote char

--- source-mapping/js/main.js
+++ source-mapping/js/main.js
@@ -1,15 +1,15 @@
 var message = document.getElementById('message');
 
 function logMessage() {
-  console.log("message is: " + message.value);
+  console.log('message is: ' + message.value);
 }
 
-message.addEventListener("focus", function() {
-  message.value = "";
+message.addEventListener('focus', function() {
+  message.value = '';
 }, false);
 
 var logMessageButton = document.getElementById('log-message');
 
-logMessageButton.addEventListener("click", function() {
+logMessageButton.addEventListener('click', function() {
   logMessage();
 }, false);
